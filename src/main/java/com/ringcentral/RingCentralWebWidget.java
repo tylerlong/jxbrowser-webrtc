a030f7a82ae3c682399af9cf5133a82fc9fbb2e8
Tyler Long <tyler4long@gmail.com>
2017-10-23 14:45:37 +0800

    Click to dial

--- src/main/java/com/ringcentral/RingCentralWebWidget.java
+++ src/main/java/com/ringcentral/RingCentralWebWidget.java
@@ -5,15 +5,26 @@ import com.teamdev.jxbrowser.chromium.swing.BrowserView;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class RingCentralWebWidget {
     public static void main(String[] args) {
-        Browser browser = new Browser();
+        new MainApp();
+    }
+}
+
+class MainApp implements ActionListener {
+    private JTextField textField = null;
+    private Browser browser = null;
+
+    MainApp() {
+        browser = new Browser();
         BrowserView browserView = new BrowserView(browser);
 
         JPanel panel = new JPanel();
-        JButton button = new JButton("Call");
-        JTextField textField = new JTextField("Replace me with a phone number", 20);
+        JButton button = new JButton("Click to Dial");
+        button.addActionListener(this);
+        textField = new JTextField("Replace me with a phone number", 20);
         panel.add(button, BorderLayout.WEST);
         panel.add(textField, BorderLayout.CENTER);
 
@@ -21,10 +32,25 @@ public class RingCentralWebWidget {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.add(browserView, BorderLayout.CENTER);
         frame.add(panel, BorderLayout.SOUTH);
-        frame.setSize(350, 550);
+        frame.setSize(400, 700);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
         browser.loadURL("https://ringcentral.github.io/ringcentral-web-widget/app.html");
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+        String text = textField.getText();
+        if (!text.matches("^\\d{4,}$")) {
+            JOptionPane.showMessageDialog(null, "Please enter four or more digits");
+            textField.setText("");
+            textField.grabFocus();
+            return;
+        }
+        browser.executeJavaScript("window.postMessage({"
+            + "type: 'rc-adapter-new-call',"
+            + "phoneNumber: '" + text + "',"
+            + "toCall: true"
+        + "}, '*');");
     }
 }
